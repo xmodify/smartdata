c69559e57710baab9bc894bd7c13813db175c657
xmodify <xmodifya@gmail.com>
2026-04-04 13:39:33 +0700

    update v.69-04-04_1330

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -435,7 +435,7 @@
             </div>
 
             <div class="text-center pb-4 text-muted small mt-auto" style="opacity: 0.6;">
-                V. 69-04-03 18:30
+                V. 69-04-04 18:30
             </div>
         </div>
         <!-- /#sidebar-wrapper -->
