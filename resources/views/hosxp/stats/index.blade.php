883b5d4d773e36bf1e05399e5b4844812cdc3bf2
xmodify <xmodifya@gmail.com>
2026-05-25 21:49:17 +0700

    update

--- resources/views/hosxp/stats/index.blade.php
+++ resources/views/hosxp/stats/index.blade.php
@@ -172,11 +172,7 @@
                         <a href="{{ route('hosxp.er.index') }}" class="list-group-item list-group-item-action border-0 px-0 py-2 d-flex justify-content-between align-items-center">
                             <span class="small">รายงานข้อมูลบริการผู้ป่วยอุบัติเหตุ-ฉุกเฉิน</span>
                             <i class="fas fa-chevron-right smaller text-muted" style="font-size: 0.7rem;"></i>
-                        </a>
-                        <a href="#" class="list-group-item list-group-item-action border-0 px-0 py-2 d-flex justify-content-between align-items-center text-muted">
-                            <span class="small">รายงานจำนวนผู้ป่วยอุบัติเหตุ-ฉุกเฉิน แยกระดับความรุนแรง</span>
-                            <i class="fas fa-chevron-right smaller" style="font-size: 0.7rem;"></i>
-                        </a>
+                        </a>                        
                         <a href="#" class="list-group-item list-group-item-action border-0 px-0 py-2 d-flex justify-content-between align-items-center text-muted">
                             <span class="small">รายงานผู้ป่วยรอ Admit ที่ ER เกิน 2 ชั่วโมง</span>
                             <i class="fas fa-chevron-right smaller" style="font-size: 0.7rem;"></i>
